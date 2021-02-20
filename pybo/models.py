0296b2217b317ab4a183fbfcba9c8b377631c626
developer-Wonjun <iwonjun97@naver.com>
2021-02-20 20:49:25 +0900

    djagno tutorial 3

---
+++ pybo/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Question(models.Model):
+    subject = models.CharField(max_length=200)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+    def __str__(self):   #shell에서 id값이 제목 그대로 표기되기 위해서!!
+        return self.subject
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+
+
