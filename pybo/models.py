739702299be1f6a509c40af93a1218d1aadfc205
developer-Wonjun <iwonjun97@naver.com>
2021-02-24 20:48:19 +0900

    D-one site building day-5

--- pybo/models.py
+++ pybo/models.py
@@ -19,4 +19,12 @@ class Answer(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
 
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE) #댓글 글쓴이
+    content = models.TextField()# 댓글 내용
+    create_date = models.DateTimeField()#댓글 작성일시
+    modify_date = models.DateTimeField(null=True, blank=True)#댓글 수정일시
+    question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE)#이 댓글이 달린 질문
+    answer = models.ForeignKey(Answer, null=True, blank=True, on_delete=models.CASCADE)# 이 댓글이 달린 답변
+
 
--- pybo/views.py
+++ pybo/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
-from .models import Question
+from .models import Question, Answer
 from django.utils import timezone
-from .forms import QuestionForm, AnswerForm
+from .forms import QuestionForm, AnswerForm, CommentForm
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -110,4 +110,43 @@ def question_delete(request, question_id):
         return redirect('pybo:detail', question_id = question.id)
     question.delete()
     return redirect('pybo:index')
+
+@login_required(login_url='common:login') #로그인 상태인지 검사한후 비로그인 상태면 로그인창으로 이동
+def answer_modify(request, answer_id):
+    """
+    pybo 답변 수정
+    """
+    answer = get_object_or_404(Answer, pk=answer_id)
+
+    if request.user != answer.author:
+        messages.error(request, '수정권한이 없습니다.')
+        return redirect('pybo:detail', question_id = question.id)
+
+    if request.method == 'POST':
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.author = request.user
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect('pybo:detail', question_id=answer.question.id)
+    else:
+        form = AnswerForm(instance=answer)
+    context= {'answer':answer, 'form':form}
+    return render(request, 'pybo/answer_form.html', context)
+
+
+@login_required(login_url='common:login') #로그인 상태인지 검사한후 비로그인 상태면 로그인창으로 이동
+def answer_delete(request, answer_id):
+    """
+    pybo 답변 삭제
+    """
+    answer= get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '삭제권한이 없습니다.')
+    
+    else:
+        answer.delete()
+    return redirect('pybo:detail', question_id = answer.question.id)
+
 
