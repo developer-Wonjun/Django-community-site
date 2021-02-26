c4476b2a638b05cfa7cd4d05d18733b05d386fdc
developer-Wonjun <iwonjun97@naver.com>
2021-02-26 21:40:02 +0900

    Add) Question/Answer recommendation function

--- pybo/models.py
+++ pybo/models.py
@@ -3,21 +3,24 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Question(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')
     modify_date = models.DateTimeField(null=True, blank=True) #수정 일시를 의미. null,blank = 어떤조건으로든 값을 비어둘 수 있음을 의미. 
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
+    voter = models.ManyToManyField(User, related_name='voter_question')
 
     def __str__(self):   #shell에서 id값이 제목 그대로 표기되기 위해서!!
         return self.subject
 
 class Answer(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
     modify_date = models.DateTimeField(null=True, blank=True) #수정 일시를 의미. null,blank = 어떤조건으로든 값을 비어둘 수 있음을 의미. 
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
+    voter = models.ManyToManyField(User, related_name='voter_answer')
+
 
 class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE) #댓글 글쓴이
